2afcd37c0f8f20f57f1fdbc5f45c174d4c397cbd
Buddhimal Gunasekara <bnawgunasekara@gmail.com>
2020-07-18 00:35:14 +0530

    Add Missing Library

--- application/libraries/Messagesender.php
+++ application/libraries/Messagesender.php
@@ -5,6 +5,24 @@ include("Newsletterslk_SMS/newsletterslk.class.php");
 class Messagesender
 {
 
+	public function send_otp($Mobile, $TEXT)
+	{
+		$newsletters = new Newsletterslk;
+		$newsletters->setUser(APIKeys::SMS_API_KEY, APIKeys::SMS_API_TOKEN);// Initializing User Api Key and Api Token
+		$newsletters->setSenderID(APIKeys::SMS_SENDER_ID);// Initializing Sender ID
+		$newsletters->msgType = 'sms';
+		$newsletters->file = '';            //Set to default
+		$newsletters->language = '';        //Set to default
+		$newsletters->scheduledate = '';    //Set to default
+		$newsletters->duration = '';        //Set to default
+
+		$TEXT = 'Your OTP code is : ' . $TEXT;
+
+		if ($newsletters->SendMessage($Mobile, $TEXT, FALSE))
+			return true;
+		else
+			return false;
+	}
 
     public function send_otp($Mobile, $TEXT)
     {
