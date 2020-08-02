3c77cc9b965e9e30cf6bcfb3269b49e5c4153261
Buddhimal Gunasekara <bnawgunasekara@gmail.com>
2020-08-03 02:28:07 +0530

    check whether public record from clinic or not

--- application/models/Mclinicappointment.php
+++ application/models/Mclinicappointment.php
@@ -84,7 +84,7 @@ class Mclinicappointment extends CI_Model
 			$this->post['patient_id'] = $patient_id;
 			$this->post['is_canceled'] = 0;
 			$this->post['appointment_status'] = AppointmentStatus::PENDING;
-			if ($patient->is_clinic)
+			if ($patient["is_clinic"])
 				$this->post['appointment_charge'] = 0;
 			$this->post['appointment_charge'] = Payments::DEFAULT_CHARGE;
 			$this->post['appointment_status_updated'] = date("Y-m-d H:i:s");
